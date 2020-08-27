11e5e34b508a14f29f21a017d25c64923f4ec499
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-08-27 15:48:11 +0200

    Ass2 Task1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -32,7 +33,7 @@ class Main extends Component {
             return(
                 <DishDetail dish={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId))[0]}
                             comments={this.state.comments.filter(comment => comment.dishId === parseInt(match.params.dishId))} />
-            )
+            )  
         }
 
         const HomePage= () => {
@@ -53,6 +54,7 @@ class Main extends Component {
                         <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                         <Route path='/menu/:dishId' component={DishWithId} />
                         <Route exact path='/contactus' component={Contact} />
+                        <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                         <Redirect to='/home' />
                     </Switch>
                 <Footer />
