dc7a4872d0ed5008dab02e0810f329d8b084da5c
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-08-27 12:40:14 +0200

    Ass1 Task3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,28 +8,44 @@ class DishDetail extends Component {
 
     renderDish(dish) {
         return(
-            <Card>
-                <CardImg width='100%' src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-            </Card>
+            <div className='col-12 col-md-5 m-1'>
+                <Card>
+                    <CardImg width='100%' src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </div>
         );
+    }
+
+    renderComments(comments) {
+        if(comments == []) return(<div></div>)
+        return(
+            <div className='col-12 col-md-5 m-1'>
+                <h4><strong>Comments</strong></h4>
+                <ul className='list-unstyled'>
+                    {comments.map(eachComment => {
+                        return(<p>{eachComment.comment}<br/>
+                        -- <em>{eachComment.author}</em>, <span>{new Date(eachComment.date).toLocaleDateString()}</span>
+                        </p>)
+                    })}
+                </ul>
+            </div>
+        )
     }
 
     render() {
         const dish = this.props.dish;
+        let comments = [];
+        (dish == null) ? comments = [] : comments = dish.comments;
 
         if(dish == null) return(<div></div>);
         return(
             <div className='row'>
-            <div className='col-12 col-md-5 mt-1'>
                 {this.renderDish(dish)}
+                {this.renderComments(comments)}
             </div>
-            <div className='col-12 col-md-5 mt-1'>
-                
-            </div>
-        </div>
         )
     }
 }
