b06edb50c775bcf7e69b50a7d6f416def123e3c4
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-08-31 15:36:29 +0200

    Ass3 Task2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { Card, CardImg, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader } from 'reactstrap';
+import { Card, CardImg, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control } from 'react-redux-form';
 
 class CommentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isModalOpen: false
+            isModalOpen: false,
+            rating: 1,
+            author: '',
+            comment: ''
         };
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     toggleModal() {
@@ -17,11 +22,46 @@ class CommentForm extends Component {
         })
     }
 
+    handleSubmit(values) {
+        console.log(JSON.stringify(values));
+        alert(JSON.stringify(values));
+
+    }
     render() {
         return(
             <div>
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                            <Row className='form-group'>
+                                <Label htmlFor='rating' md={2}>Rating</Label>
+                                <Control.select model='.rating' id='rating'>
+                                    <option value='1'>1</option>
+                                    <option value='2'>2</option>
+                                    <option value='3'>3</option>
+                                    <option value='4'>4</option>
+                                    <option value='5'>5</option>
+                                </Control.select>
+                            </Row>
+                            <Row className='form-group'>
+                                <Label htmlFor='author' md={2}>Your name</Label>
+                                <Control.text model='.author' id='author' />
+                            </Row>
+                            <Row className='form-group'>
+                                <Label htmlFor='comment' md={2}>Your comment</Label>
+                                <Control.textarea model='.comment' id='comment' />
+                            </Row>
+                            <Row className='form-group'>
+                                <Col md={{
+                                    size: 10,
+                                    offset: 2
+                                }}>
+                                    <Button type='submit' color='primary'>Submit Comment</Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
                 </Modal>
 
                 <Button outline onClick={this.toggleModal}>
