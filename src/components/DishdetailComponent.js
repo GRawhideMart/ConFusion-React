bf45f0d7fc272051a9e07ea344afb8413fc13d40
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-08-31 15:54:52 +0200

    Ass3 Task3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardImg, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LocalForm, Control } from 'react-redux-form';
+import { LocalForm, Control, Errors } from 'react-redux-form';
 
-class CommentForm extends Component {
+export class CommentForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -28,15 +28,20 @@ class CommentForm extends Component {
 
     }
     render() {
+        const required = (val) => val && val.length;
+        const maxLength = (len) => (val) => !(val) || (val.length <= len);
+        const minLength = (len) => (val) => val && (val.length >= len);
+        
         return(
             <div>
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal} modalTransition={{ timeout: 250 }} backdropTransition={{ timeout: 250 }}>
                     <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
                     <ModalBody>
                         <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className='form-group'>
                                 <Label htmlFor='rating' md={2}>Rating</Label>
-                                <Control.select model='.rating' id='rating'>
+                                <Control.select model='.rating' id='rating'
+                                    className='form-control'>
                                     <option value='1'>1</option>
                                     <option value='2'>2</option>
                                     <option value='3'>3</option>
@@ -46,7 +51,22 @@ class CommentForm extends Component {
                             </Row>
                             <Row className='form-group'>
                                 <Label htmlFor='author' md={2}>Your name</Label>
-                                <Control.text model='.author' id='author' />
+                                <Control.text model='.author' id='author' 
+                                              className='form-control'
+                                              validators={{
+                                                  required,
+                                                  minLength: minLength(2),
+                                                  maxLength: maxLength(15)
+                                              }}/>
+                                <Errors
+                                    className='text-danger'
+                                    model='.author'
+                                    show='touched'
+                                    messages={{
+                                        required: 'Required\n',
+                                        minLength: 'Must be longer than 2 characters',
+                                        maxLength: 'Must be shorter than 15 characters'
+                                    }}/>
                             </Row>
                             <Row className='form-group'>
                                 <Label htmlFor='comment' md={2}>Your comment</Label>
